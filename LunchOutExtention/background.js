2966a3cff82263273c89e0cdcb9da692c6b4675a
David Spriggs <spriggs.d@gmail.com>
2014-08-27 10:34:56 -0500

    Fixing high cpu.

--- LunchOutExtention/background.js
+++ LunchOutExtention/background.js
@@ -83,9 +83,6 @@
 				}
 
 				callback(orders, count);
-			} else {
-				//fire off to check again
-				timer = setTimeout(triggerStatusCheck, 10000);
 			}
 		};
 		xmlhttp.open("GET", ordersServer + "/orders/count", true);
